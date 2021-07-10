33bd31361e3160f08dab4c5f3746f7b2741d2e29
Gabriel Augusto <biga00145@gmail.com>
2021-07-10 18:57:12 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/projeto/decrypter/annotation/ProjetoDecrypterAnnotationApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.decrypter.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDecrypterAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDecrypterAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/decrypter/annotation/ProjetoDecrypterAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.decrypter.annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDecrypterAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
